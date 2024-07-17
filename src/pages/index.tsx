c5773d1a27101d9128e28ea892a0a7493471ca31
Noah Mulfinger <nmulfinger@esri.com>
2024-07-17 13:44:06 -0700

    Add calcite components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,128 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import * as React from "react";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import "@esri/calcite-components/dist/calcite/calcite.css";
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+// Import all components to test SSR
+import "@esri/calcite-components/dist/components/calcite-accordion-item";
+import "@esri/calcite-components/dist/components/calcite-accordion";
+import "@esri/calcite-components/dist/components/calcite-action-bar";
+import "@esri/calcite-components/dist/components/calcite-action-group";
+import "@esri/calcite-components/dist/components/calcite-action-menu";
+import "@esri/calcite-components/dist/components/calcite-action-pad";
+import "@esri/calcite-components/dist/components/calcite-action";
+import "@esri/calcite-components/dist/components/calcite-alert";
+import "@esri/calcite-components/dist/components/calcite-avatar";
+import "@esri/calcite-components/dist/components/calcite-block-section";
+import "@esri/calcite-components/dist/components/calcite-block";
+import "@esri/calcite-components/dist/components/calcite-button";
+import "@esri/calcite-components/dist/components/calcite-card-group";
+import "@esri/calcite-components/dist/components/calcite-card";
+import "@esri/calcite-components/dist/components/calcite-carousel-item";
+import "@esri/calcite-components/dist/components/calcite-carousel";
+import "@esri/calcite-components/dist/components/calcite-checkbox";
+import "@esri/calcite-components/dist/components/calcite-chip-group";
+import "@esri/calcite-components/dist/components/calcite-chip";
+import "@esri/calcite-components/dist/components/calcite-color-picker-hex-input";
+import "@esri/calcite-components/dist/components/calcite-color-picker-swatch";
+import "@esri/calcite-components/dist/components/calcite-color-picker";
+import "@esri/calcite-components/dist/components/calcite-combobox-item-group";
+import "@esri/calcite-components/dist/components/calcite-combobox-item";
+import "@esri/calcite-components/dist/components/calcite-combobox";
+import "@esri/calcite-components/dist/components/calcite-date-picker-day";
+import "@esri/calcite-components/dist/components/calcite-date-picker-month-header";
+import "@esri/calcite-components/dist/components/calcite-date-picker-month";
+import "@esri/calcite-components/dist/components/calcite-date-picker";
+import "@esri/calcite-components/dist/components/calcite-dropdown-group";
+import "@esri/calcite-components/dist/components/calcite-dropdown-item";
+import "@esri/calcite-components/dist/components/calcite-dropdown";
+import "@esri/calcite-components/dist/components/calcite-fab";
+import "@esri/calcite-components/dist/components/calcite-filter";
+import "@esri/calcite-components/dist/components/calcite-flow-item";
+import "@esri/calcite-components/dist/components/calcite-flow";
+import "@esri/calcite-components/dist/components/calcite-graph";
+import "@esri/calcite-components/dist/components/calcite-handle";
+import "@esri/calcite-components/dist/components/calcite-icon";
+import "@esri/calcite-components/dist/components/calcite-inline-editable";
+import "@esri/calcite-components/dist/components/calcite-input-date-picker";
+import "@esri/calcite-components/dist/components/calcite-input-message";
+import "@esri/calcite-components/dist/components/calcite-input-number";
+import "@esri/calcite-components/dist/components/calcite-input-text";
+import "@esri/calcite-components/dist/components/calcite-input-time-picker";
+import "@esri/calcite-components/dist/components/calcite-input-time-zone";
+import "@esri/calcite-components/dist/components/calcite-input";
+import "@esri/calcite-components/dist/components/calcite-label";
+import "@esri/calcite-components/dist/components/calcite-link";
+import "@esri/calcite-components/dist/components/calcite-list-item-group";
+import "@esri/calcite-components/dist/components/calcite-list-item";
+import "@esri/calcite-components/dist/components/calcite-list";
+import "@esri/calcite-components/dist/components/calcite-loader";
+import "@esri/calcite-components/dist/components/calcite-menu-item";
+import "@esri/calcite-components/dist/components/calcite-menu";
+import "@esri/calcite-components/dist/components/calcite-meter";
+import "@esri/calcite-components/dist/components/calcite-modal";
+import "@esri/calcite-components/dist/components/calcite-navigation-logo";
+import "@esri/calcite-components/dist/components/calcite-navigation-user";
+import "@esri/calcite-components/dist/components/calcite-navigation";
+import "@esri/calcite-components/dist/components/calcite-notice";
+import "@esri/calcite-components/dist/components/calcite-option-group";
+import "@esri/calcite-components/dist/components/calcite-option";
+import "@esri/calcite-components/dist/components/calcite-pagination";
+import "@esri/calcite-components/dist/components/calcite-panel";
+import "@esri/calcite-components/dist/components/calcite-pick-list-group";
+import "@esri/calcite-components/dist/components/calcite-pick-list-item";
+import "@esri/calcite-components/dist/components/calcite-pick-list";
+import "@esri/calcite-components/dist/components/calcite-popover";
+import "@esri/calcite-components/dist/components/calcite-progress";
+import "@esri/calcite-components/dist/components/calcite-radio-button-group";
+import "@esri/calcite-components/dist/components/calcite-radio-button";
+import "@esri/calcite-components/dist/components/calcite-rating";
+import "@esri/calcite-components/dist/components/calcite-scrim";
+import "@esri/calcite-components/dist/components/calcite-segmented-control-item";
+import "@esri/calcite-components/dist/components/calcite-segmented-control";
+import "@esri/calcite-components/dist/components/calcite-select";
+import "@esri/calcite-components/dist/components/calcite-sheet";
+import "@esri/calcite-components/dist/components/calcite-shell-center-row";
+import "@esri/calcite-components/dist/components/calcite-shell-panel";
+import "@esri/calcite-components/dist/components/calcite-shell";
+import "@esri/calcite-components/dist/components/calcite-slider";
+import "@esri/calcite-components/dist/components/calcite-sortable-list";
+import "@esri/calcite-components/dist/components/calcite-split-button";
+import "@esri/calcite-components/dist/components/calcite-stack";
+import "@esri/calcite-components/dist/components/calcite-stepper-item";
+import "@esri/calcite-components/dist/components/calcite-stepper";
+import "@esri/calcite-components/dist/components/calcite-switch";
+import "@esri/calcite-components/dist/components/calcite-tab-nav";
+import "@esri/calcite-components/dist/components/calcite-tab-title";
+import "@esri/calcite-components/dist/components/calcite-tab";
+import "@esri/calcite-components/dist/components/calcite-table-cell";
+import "@esri/calcite-components/dist/components/calcite-table-header";
+import "@esri/calcite-components/dist/components/calcite-table-row";
+import "@esri/calcite-components/dist/components/calcite-table";
+import "@esri/calcite-components/dist/components/calcite-tabs";
+import "@esri/calcite-components/dist/components/calcite-text-area";
+import "@esri/calcite-components/dist/components/calcite-tile-group";
+import "@esri/calcite-components/dist/components/calcite-tile-select-group";
+import "@esri/calcite-components/dist/components/calcite-tile-select";
+import "@esri/calcite-components/dist/components/calcite-tile";
+import "@esri/calcite-components/dist/components/calcite-time-picker";
+import "@esri/calcite-components/dist/components/calcite-tip-group";
+import "@esri/calcite-components/dist/components/calcite-tip-manager";
+import "@esri/calcite-components/dist/components/calcite-tip";
+import "@esri/calcite-components/dist/components/calcite-tooltip";
+import "@esri/calcite-components/dist/components/calcite-tree-item";
+import "@esri/calcite-components/dist/components/calcite-tree";
+import "@esri/calcite-components/dist/components/calcite-value-list-item";
+import "@esri/calcite-components/dist/components/calcite-value-list";
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
+import { CalciteSlider } from "@esri/calcite-components-react";
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-const IndexPage: React.FC<PageProps> = () => {
+const IndexPage: React.FC = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+    <main>
+      <h1>Calcite components integration</h1>
+      <CalciteSlider />
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
-
-export const Head: HeadFC = () => <title>Home Page</title>
+export default IndexPage;
